6cbf64df97189bee1b5912f58fc563ae6bb2d43e
hachemmosbah <hachem.mosbah.data@gmail.com>
2023-07-30 13:36:40 +0200

    update AI.py and add .gitignore

--- IA.py
+++ IA.py
@@ -1,6 +1,26 @@
-#!/usr/bin/env python
+# !usr/bin/env python
+
+# importer des bibiliothèques 
 import openai
 from configparser import ConfigParser
+import os
+
+def create_config(keyapi):
+  """
+  Creates a configuration file with the provided API key.
+
+  Parameters:
+      keyapi (str): The API key to be stored in the configuration file.
+  """
+  config = ConfigParser()  
+  config["openai"]={
+    "api_key" : f"{keyapi}", 
+  }
+  with open(r"AI_config.cfg", 'w') as f:
+    config.write(f)
+    
+
+
 def get_api_key():
   """
   Reads the API key from the configuration file and returns it.
@@ -9,18 +29,19 @@ def get_api_key():
     str: The API key read from the configuration file.
   """
   config = ConfigParser()
-  config.read('config/AI_config.cfg')
+  config.read('AI_config.cfg')
 
   API_KEY = config.get('openai', 'api_key')
   return API_KEY
 
-openai.api_key = get_api_key()
 
+# Un dictionnaire contenant les choix de questions
 Choix = {
     "1" : "Corrige moi le code suivant sans explication",
     "2" : "Optimise moi le code suivant sans explication",
     "3" : "Commente moi le code suivant sans explication"
 }
+
 def IA(func):    
     """
     Decorator function that wraps another function `func` and returns a new function `inner1`.
@@ -59,18 +80,34 @@ def chatGptAsk(question, selection):
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       messages=[
-        {"role": "system", "content": Choix[selection]},
+        {"role": "system", "content": Choix[str(selection)]},
         {"role": "user", "content": question}
       ]
     )
     return response.choices[0].message.content + "\n"
 
-selection= input("selection entre : \n \
-                1 : Corrige moi le code suivant sans explication, \n \
-                2 : Optimise moi le code suivant sans explication \n \
-                3 : Commente moi le code suivant sans explication \n")
-question = input("Entrez votre code :")
 
-print("asking:", chatGptAsk(question, selection))
+def main():
+    if os.path.exists("AI_config.cfg"):
+      openai.api_key = get_api_key()
+    else:
+      keyapi = input("Entrez votre API key : \n")
+      create_config(keyapi)
+      openai.api_key = get_api_key()
+
+    while True:
+        selection= int(input("selection entre : \n \
+                  1 : Corrige moi le code suivant sans explication, \n \
+                  2 : Optimise moi le code suivant sans explication \n \
+                  3 : Commente moi le code suivant sans explication \n \
+                  4 :  Quiter le programme \n "))
+        if selection == 4:
+          print("Au revoir à bientôt")
+          return 
+          
+        question = input("Entrez votre code : \n")
+        print(chatGptAsk(question, selection))
 
 
+if __name__ == "__main__":   
+    main()
